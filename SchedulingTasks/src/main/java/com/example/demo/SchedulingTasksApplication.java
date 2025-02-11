57765a9cfc2d23b7af0f62795700658789c68169
Sneha7890 <sneofficialid@gmail.com>
2025-02-11 13:21:42 +0530

    Day9

---
+++ SchedulingTasks/src/main/java/com/example/demo/SchedulingTasksApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@ComponentScan
+@EnableAutoConfiguration
+@Configuration
+@EnableScheduling
+@SpringBootApplication
+public class SchedulingTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTasksApplication.class, args);
+	}
+
+}
