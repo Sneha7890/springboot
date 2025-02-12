fb9d63361307535fca424105978b3da443747e12
Sneha7890 <sneofficialid@gmail.com>
2025-02-12 18:29:12 +0530

    Day10

---
+++ SBA-Message/src/main/java/com/example/demo/SbaMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaMessageApplication.class, args);
+	}
+
+}
